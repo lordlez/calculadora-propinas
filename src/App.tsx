f7fabd17c4e9bc3164742904f2b197800b3a44cf
Lorenzo Lezcano <lorenzodlezcano@gmail.com>
2024-12-13 22:40:58 -0300

    add data base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { menuItems } from "./data/db";
+import { MenuItems } from "./components/MenuItems";
+
 function App() {
+  console.log(menuItems);
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4x1 font-black">
+        <h1 className="text-center text-4xl font-black">
           Calculadora de Propinas y Consumo
         </h1>
       </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItems key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   );
 }
